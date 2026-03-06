c32cbed87c4ce2f3c6044e364dc9fb1e4cbaac96
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-03-06 15:20:33 -0500

    updating odin disko

--- modules/machines/nixos/odin/disko.nix
+++ modules/machines/nixos/odin/disko.nix
@@ -4,8 +4,8 @@ let
   devices = config.zfs-root.bootDevices or [ "ata-CT500MX500SSD1_1947E228A4C0" ];
   diskMain = builtins.elemAt devices 0;
 
-  # SnapRAID Drives (5.5T each)
-  parityDisk = "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D57REZ0V"; # sdc
+  # 6TB WDC Drives
+  parityDisk = "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D57REZ0V"; 
   dataDisks = [
     "ata-WDC_WD60EDAZ-11U78B0_WD-WX92D62J3FRL" # sdd
     "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D28H9YHC" # sde
@@ -16,68 +16,113 @@ in
 {
   disko.devices = {
     disk = {
-      # --- EXISTING OS DRIVE ---
+      # --- OS SSD (ZFS) ---
       main = {
         type = "disk";
         device = "/dev/disk/by-id/${diskMain}";
         content = {
           type = "gpt";
           partitions = {
-            efi = { size = "1G"; type = "EF00"; content = { type = "filesystem"; format = "vfat"; mountpoint = "/boot/efis/${diskMain}-part2"; }; };
+            boot = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
             bpool = { size = "4G"; content = { type = "zfs"; pool = "bpool"; }; };
-            rpool = { end = "-1M"; content = { type = "zfs"; pool = "rpool"; }; };
-            bios = { size = "100%"; type = "EF02"; };
+            rpool = { size = "100%"; content = { type = "zfs"; pool = "rpool"; }; };
           };
         };
       };
 
-      # --- NEW SNAPRAID DRIVES ---
+      # --- Parity HDD (XFS) ---
       parity1 = {
         type = "disk";
         device = "/dev/disk/by-id/${parityDisk}";
-        content = { type = "gpt"; partitions.zfs = { size = "100%"; content = { type = "zfs"; pool = "p_d1"; }; }; };
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/mnt/parity1";
+            };
+          };
+        };
       };
-      data1 = { type = "disk"; device = "/dev/disk/by-id/${builtins.elemAt dataDisks 0}"; content = { type = "gpt"; partitions.zfs = { size = "100%"; content = { type = "zfs"; pool = "d_d1"; }; }; }; };
-      data2 = { type = "disk"; device = "/dev/disk/by-id/${builtins.elemAt dataDisks 1}"; content = { type = "gpt"; partitions.zfs = { size = "100%"; content = { type = "zfs"; pool = "d_d2"; }; }; }; };
-      data3 = { type = "disk"; device = "/dev/disk/by-id/${builtins.elemAt dataDisks 2}"; content = { type = "gpt"; partitions.zfs = { size = "100%"; content = { type = "zfs"; pool = "d_d3"; }; }; }; };
-      data4 = { type = "disk"; device = "/dev/disk/by-id/${builtins.elemAt dataDisks 3}"; content = { type = "gpt"; partitions.zfs = { size = "100%"; content = { type = "zfs"; pool = "d_d4"; }; }; }; };
+
+      # --- Data HDDs (XFS) ---
+      data1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/${builtins.elemAt dataDisks 0}";
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = { type = "filesystem"; format = "xfs"; mountpoint = "/mnt/data1"; };
+          };
+        };
+      };
+      data2 = {
+        type = "disk";
+        device = "/dev/disk/by-id/${builtins.elemAt dataDisks 1}";
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = { type = "filesystem"; format = "xfs"; mountpoint = "/mnt/data2"; };
+          };
+        };
+      };
+      data3 = {
+        type = "disk";
+        device = "/dev/disk/by-id/${builtins.elemAt dataDisks 2}";
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = { type = "filesystem"; format = "xfs"; mountpoint = "/mnt/data3"; };
+          };
+        };
+      };
+      data4 = {
+        type = "disk";
+        device = "/dev/disk/by-id/${builtins.elemAt dataDisks 3}";
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = { type = "filesystem"; format = "xfs"; mountpoint = "/mnt/data4"; };
+          };
+        };
+      };
     };
 
+    # --- ZFS Pool Definitions (OS Only) ---
     zpool = {
-      # bpool and rpool configurations remain as they were in your snippet
       bpool = {
         type = "zpool";
-        options = { ashift = "12"; autotrim = "on"; compatibility = "grub2"; };
-        mountpoint = "/boot";
+        mode = "";
         datasets = {
-          nixos = { type = "zfs_fs"; options.mountpoint = "none"; };
-          "nixos/root" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/boot"; };
+          "nixos" = { type = "zfs_fs"; mountpoint = null; };
+          "nixos/root" = { type = "zfs_fs"; mountpoint = "/boot"; options.mountpoint = "legacy"; };
         };
       };
-
-      rpool = {
+      rpool = { 
         type = "zpool";
-        options = { ashift = "12"; autotrim = "on"; };
-        mountpoint = "/";
+        mode = "";
         datasets = {
-          nixos = { type = "zfs_fs"; options.mountpoint = "none"; };
-          "nixos/empty" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/"; postCreateHook = "zfs snapshot rpool/nixos/empty@start"; };
-          "nixos/home" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/home"; };
-          "nixos/data" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/mnt/user"; };
-          "nixos/var/log" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/var/log"; };
-          "nixos/var/lib" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/var/lib"; };
-          "nixos/config" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/etc/nixos"; };
-          "nixos/persist" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/persist"; };
-          "nixos/nix" = { type = "zfs_fs"; options.mountpoint = "legacy"; mountpoint = "/nix"; };
+          "nixos" = { type = "zfs_fs"; mountpoint = null; };
+          "nixos/empty" = { type = "zfs_fs"; mountpoint = "/"; options.mountpoint = "legacy"; };
+          "nixos/home" = { type = "zfs_fs"; mountpoint = "/home"; options.mountpoint = "legacy"; };
+          "nixos/nix" = { type = "zfs_fs"; mountpoint = "/nix"; options.mountpoint = "legacy"; };
+          "nixos/persist" = { type = "zfs_fs"; mountpoint = "/persist"; options.mountpoint = "legacy"; };
         };
       };
-
-      # New SnapRAID Pools
-      p_d1 = { type = "zpool"; mountpoint = "/mnt/parity1"; datasets.storage = { type = "zfs_dataset"; mountpoint = "/mnt/parity1/storage"; }; };
-      d_d1 = { type = "zpool"; mountpoint = "/mnt/data1";   datasets.storage = { type = "zfs_dataset"; mountpoint = "/mnt/data1/storage"; }; };
-      d_d2 = { type = "zpool"; mountpoint = "/mnt/data2";   datasets.storage = { type = "zfs_dataset"; mountpoint = "/mnt/data2/storage"; }; };
-      d_d3 = { type = "zpool"; mountpoint = "/mnt/data3";   datasets.storage = { type = "zfs_dataset"; mountpoint = "/mnt/data3/storage"; }; };
-      d_d4 = { type = "zpool"; mountpoint = "/mnt/data4";   datasets.storage = { type = "zfs_dataset"; mountpoint = "/mnt/data4/storage"; }; };
     };
   };
 }
