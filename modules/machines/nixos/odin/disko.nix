6cdf4bcd362c8c3962b9d87a7e4fa73d9e681fcf
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-03-17 13:41:18 -0400

    odin disko update

--- modules/machines/nixos/odin/disko.nix
+++ modules/machines/nixos/odin/disko.nix
@@ -1,23 +1,22 @@
-{ config, ... }:
+{ config ? { }, ... }:
 let
-  # OS Drives (Crucial SSDs)
-  bootDrives = [
+  bootDrives = config.zfs-root.bootDevices or [
     "ata-CT500MX500SSD1_1947E228A4C0"
     "ata-CT500MX500SSD1_1947E228A5E2"
   ];
 
-  # Storage Drives (WD 6TB)
-  storageDrives = [
-    "ata-WDC_WD60EDAZ-11U78B0_WD-WX22A82EZPTC"
-    "ata-WDC_WD60EDAZ-11U78B0_WD-WX52DC0KY6JR"
-    "ata-WDC_WD60EDAZ-11U78B0_WD-WX92D62J3FRL"
-    "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D28H9YHC"
-    "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D57REZ0V"
+  # Mapping the specific IDs to your required labels
+  # 4 Data drives + 1 Parity drive
+  dataDiskIds = [
+    { id = "ata-WDC_WD60EDAZ-11U78B0_WD-WX22A82EZPTC"; label = "Data1"; }
+    { id = "ata-WDC_WD60EDAZ-11U78B0_WD-WX52DC0KY6JR"; label = "Data2"; }
+    { id = "ata-WDC_WD60EDAZ-11U78B0_WD-WX92D62J3FRL"; label = "Data3"; }
+    { id = "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D28H9YHC"; label = "Data4"; }
+    { id = "ata-WDC_WD60EFRX-68L0BN1_WD-WX11D57REZ0V"; label = "Parity1"; }
   ];
 in
 {
   disko.devices = {
-    # Generate Boot Disk configurations
     disk = (builtins.listToAttrs (builtins.genList (i: {
       name = "boot${toString i}";
       value = {
@@ -34,7 +33,6 @@ in
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot/efis/boot${toString i}";
-                mountOptions = [ "umask=0077" ];
               };
             };
             zfs = {
@@ -45,77 +43,49 @@ in
         };
       };
     }) (builtins.length bootDrives))) // 
-    # Generate Storage Disk configurations
-    (builtins.listToAttrs (builtins.genList (i: {
-      name = "storage${toString i}";
+    # Generate XFS Data Disks
+    (builtins.listToAttrs (map (item: {
+      name = item.label;
       value = {
         type = "disk";
-        device = "/dev/disk/by-id/${builtins.elemAt storageDrives i}";
+        device = "/dev/disk/by-id/${item.id}";
         content = {
           type = "gpt";
           partitions = {
-            zfs = {
+            primary = {
               size = "100%";
-              content = { type = "zfs"; pool = "dpool"; };
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                # Disko will apply the label here
+                extraArgs = [ "-L" item.label ];
+              };
             };
           };
         };
       };
-    }) (builtins.length storageDrives)));
+    }) dataDiskIds));
 
-    zpool = {
-      rpool = {
-        type = "zpool";
-        mode = "mirror";
-        options = { ashift = "12"; autotrim = "on"; };
-        rootFsOptions = {
-          acltype = "posixacl";
-          dnodesize = "auto";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-          mountpoint = "none";
-          canmount = "off";
-        };
-        datasets = {
-          "nixos" = { type = "zfs_fs"; options.mountpoint = "none"; };
-          "nixos/root" = {
-            type = "zfs_fs";
-            mountpoint = "/";
-            options.mountpoint = "legacy";
-            postCreateHook = "zfs snapshot rpool/nixos/root@blank";
-          };
-          "nixos/nix" = { type = "zfs_fs"; mountpoint = "/nix"; options.mountpoint = "legacy"; };
-          "nixos/home" = { type = "zfs_fs"; mountpoint = "/home"; options.mountpoint = "legacy"; };
-          "nixos/persist" = { type = "zfs_fs"; mountpoint = "/persist"; options.mountpoint = "legacy"; };
-          "nixos/var_log" = { type = "zfs_fs"; mountpoint = "/var/log"; options.mountpoint = "legacy"; };
-          "nixos/var_lib" = { type = "zfs_fs"; mountpoint = "/var/lib"; options.mountpoint = "legacy"; };
-        };
+    zpool.rpool = {
+      type = "zpool";
+      mode = "mirror";
+      rootFsOptions = {
+        acltype = "posixacl";
+        canmount = "off";
+        dnodesize = "auto";
+        normalization = "formD";
+        relatime = "on";
+        xattr = "sa";
+        mountpoint = "none";
       };
-
-      dpool = {
-        type = "zpool";
-        mode = "raidz"; # Single parity for 5 drives
-        options = { ashift = "12"; autotrim = "on"; };
-        rootFsOptions = {
-          compression = "zstd";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-          mountpoint = "none";
-        };
-        datasets = {
-          "data" = {
-            type = "zfs_fs";
-            mountpoint = "/data";
-            options.mountpoint = "legacy";
-          };
-          "media" = {
-            type = "zfs_fs";
-            mountpoint = "/data/media";
-            options.mountpoint = "legacy";
-          };
-        };
+      datasets = {
+        "nixos" = { type = "zfs_fs"; options.mountpoint = "none"; };
+        "nixos/root" = { type = "zfs_fs"; mountpoint = "/"; options.mountpoint = "legacy"; postCreateHook = "zfs snapshot rpool/nixos/root@blank"; };
+        "nixos/nix" = { type = "zfs_fs"; mountpoint = "/nix"; options.mountpoint = "legacy"; };
+        "nixos/persist" = { type = "zfs_fs"; mountpoint = "/persist"; options.mountpoint = "legacy"; };
+        "nixos/home" = { type = "zfs_fs"; mountpoint = "/home"; options.mountpoint = "legacy"; };
+        "nixos/var_log" = { type = "zfs_fs"; mountpoint = "/var/log"; options.mountpoint = "legacy"; };
+        "nixos/var_lib" = { type = "zfs_fs"; mountpoint = "/var/lib"; options.mountpoint = "legacy"; };
       };
     };
   };
