a29a0aa312c95e8dfe75b12728a9de97c4e1acdb
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-03-18 09:28:44 -0400

    fixed disko for odin

--- modules/machines/nixos/odin/boot.nix
+++ modules/machines/nixos/odin/boot.nix
@@ -1,11 +1,12 @@
 { lib, ... }:
 {
   boot.loader = {
+    grub.enable = lib.mkForce false;
     # 1. Enable systemd-boot
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
     efi.efiSysMountPoint = lib.mkForce "/boot/efis/boot0";
-    grub.enable = lib.mkForce false;
+
 
     # 2. Mirror the configuration to both EFI partitions
     # This is a top-level loader option, NOT inside systemd-boot {}
@@ -16,5 +17,7 @@
   };
 
   # Make sure GRUB is disabled so it doesn't conflict
+  options.boot.loader.mirroredBoots = lib.mkForce [ ];
 
 }
+  
