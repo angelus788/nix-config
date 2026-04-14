c90c299f99a4fee62e67d87af298103dc5f4c3fe
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-04-14 08:45:44 -0400

    moved vaultwarden to route via frp

--- modules/homelab/services/vaultwarden/default.nix
+++ modules/homelab/services/vaultwarden/default.nix
@@ -1,12 +1,13 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   service = "vaultwarden";
   cfg = config.homelab.services.${service};
-  hl = config.homelab;
 in
 {
   options.homelab.services.${service} = {
-    enable = lib.mkEnableOption "Enable ${service}";
+    enable = lib.mkEnableOption {
+      description = "Enable ${service}";
+    };
     configDir = lib.mkOption {
       type = lib.types.str;
       default = "/var/lib/bitwarden_rs";
@@ -14,10 +15,6 @@ in
     url = lib.mkOption {
       type = lib.types.str;
       default = "pass.internalnetwork.party";
-    };
-    role = lib.mkOption {
-      type = lib.types.enum [ "client" "server" ];
-      default = "client";
     };
     homepage.name = lib.mkOption {
       type = lib.types.str;
@@ -35,66 +32,65 @@ in
       type = lib.types.str;
       default = "Services";
     };
+    role = lib.mkOption {
+      type = lib.types.enum [
+        "client"
+        "server"
+      ];
+      default = "client";
+    };
   };
-
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    # --- CLIENT CONFIGURATION (Odin) ---
-    (lib.mkIf (cfg.role == "client") {
-      systemd.tmpfiles.rules = [
-        "d ${cfg.configDir} 0750 vaultwarden vaultwarden -"
-      ];
-
-      systemd.services.vaultwarden.serviceConfig = {
-        ReadOnlyPaths = [ "/nix/store" ];
-        ReadWritePaths = [ cfg.configDir ];
-        PrivateUsers = lib.mkForce false;
-        StateDirectory = lib.mkForce (baseNameOf cfg.configDir);
-        ProtectSystem = lib.mkForce "true";
-      };
-
-      services.vaultwarden = {
-        enable = true;
-        webVaultPackage = pkgs.vaultwarden.webvault;
-        config = {
-          DOMAIN = "https://${cfg.url}";
-          DATA_FOLDER = cfg.configDir;
-          ROCKET_ADDRESS = "127.0.0.1";
-          ROCKET_PORT = 8222;
-          WEB_VAULT_ENABLED = true;
-          WEB_VAULT_FOLDER = "${pkgs.vaultwarden.webvault}/share/vaultwarden/vault";
-          EXTENDED_LOGGING = true;
-          LOG_LEVEL = "info";
+  config =
+    let
+      mkIfElse =
+        p: yes: no:
+        lib.mkMerge [
+          (lib.mkIf p yes)
+          (lib.mkIf (!p) no)
+        ];
+    in
+    mkIfElse (cfg.role == "client")
+      (lib.mkIf cfg.enable {
+        services = {
+          fail2ban-cloudflare = lib.mkIf config.services.fail2ban-cloudflare.enable {
+            jails = {
+              vaultwarden = {
+                serviceName = "vaultwarden";
+                failRegex = "^.*Username or password is incorrect. Try again. IP: <HOST>. Username: <F-USER>.*</F-USER>.$";
+              };
+            };
+          };
+          ${service} = {
+            enable = true;
+            config = {
+              DOMAIN = "https://${cfg.url}";
+              SIGNUPS_ALLOWED = false;
+              ROCKET_ADDRESS = "127.0.0.1";
+              ROCKET_PORT = 8222;
+              EXTENDED_LOGGING = true;
+              LOG_LEVEL = "warn";
+              IP_HEADER = "CF-Connecting-IP";
+            };
+          };
+          frp.settings.proxies = [
+            {
+              name = service;
+              type = "tcp";
+              localIP = config.services.${service}.config.ROCKET_ADDRESS;
+              localPort = config.services.${service}.config.ROCKET_PORT;
+              remotePort = config.services.${service}.config.ROCKET_PORT;
+            }
+          ];
         };
-      };
-
-      services.frp.settings.proxies = [
-        {
-          name = service;
-          type = "tcp";
-          localIP = "127.0.0.1";
-          localPort = 8222;
-          remotePort = 8222;
-        }
-      ];
-    })
-
-    # --- SERVER CONFIGURATION (Heimdall) ---
-    (lib.mkIf (cfg.role == "server") (lib.mkMerge [
+      })
       {
         services.caddy.virtualHosts."${cfg.url}" = {
           useACMEHost = "internalnetwork.party";
           extraConfig = ''
-            reverse_proxy http://127.0.0.1:8222
-          '';
+            	            reverse_proxy http://${config.services.${service}.config.ROCKET_ADDRESS}:${
+            	              toString config.services.${service}.config.ROCKET_PORT
+            	            }
+            	          '';
         };
-      }
-
-      # The Fix: Use setAttrByPath to hide the attribute from the static evaluator.
-      # This prevents Odin from ever trying to validate the 'fail2ban-cloudflare' path.
-      (lib.setAttrByPath [ "services" "fail2ban-cloudflare" "jails" "vaultwarden" ] {
-        serviceName = "vaultwarden";
-        failRegex = "^.*Username or password is incorrect. Try again. IP: <HOST>. Username: <F-USER>.*</F-USER>.$";
-      })
-    ]))
-  ]);
+      };
 }
