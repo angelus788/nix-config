58599ae2ea9d2fad3aff2da1293974cb6713e0b9
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-08-09 12:12:10 -0400

    updated wallpaper module for cosmic

--- modules/dots/wallpaper/default.nix
+++ modules/dots/wallpaper/default.nix
@@ -1,31 +1,41 @@
-{ config, inputs, lib, pkgs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   isLinux = !pkgs.stdenv.hostPlatform.isDarwin;
   isLinuxGui = isLinux && config.myHomeDots.enableGui;
   # Define the directory path
   wallpaperDir = "${config.home.homeDirectory}/Pictures/Wallpaper";
-in 
+in
 {
   config = lib.mkIf isLinuxGui {
-  # 1. Deploy the wallpaper files from your flake input
-  home.file."Pictures/Wallpaper" = {
-    source = inputs.wallpaper;
-    recursive = true;
-  };
+    # 1. Deploy the wallpaper files from your flake input
+    home.file."Pictures/Wallpaper" = {
+      source = inputs.wallpaper;
+      recursive = true;
+      force = true;
+    };
 
-  # 2. Generate the RON configuration file
-  # We use a multi-line string ('') to ensure the RON format is exact
-  home.file.".config/cosmic/com.system76.CosmicBackground/v1/config.ron" = {
-    text = ''
-      (
-          slideshow_enabled: true,
-          slideshow_timer: 3600,
-          shuffle: true,
-          wallpaper_path: "${wallpaperDir}",
-      )
-    '';
-    force = true; 
-  };
+    # 2. Generate the RON configuration file
+    # We use a multi-line string ('') to ensure the RON format is exact
+    home.file.".config/cosmic/com.system76.CosmicBackground/v1/all" = {
+      text = ''
+        (
+            filter_by_theme: false,
+            filter_method: Lanczos,
+            output: "all",
+            rotation_frequency: 3600,
+            sampling_method: Alphanumeric,
+            scaling_mode: Zoom,
+            source: Path("${wallpaperDir}"),
+        )
+      '';
+      force = true;
+    };
   };
 }
