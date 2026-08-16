c188b32ce6510d25f24e1947d2cc97bf1f677928
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-08-15 21:29:41 -0400

    fixed missing quotes on standalone secrets files for steamdeck and ubuntu

--- modules/machines/standalone/ubuntu/secrets.nix
+++ modules/machines/standalone/ubuntu/secrets.nix
@@ -1,19 +1,15 @@
 { config, inputs, ... }: {
   age.secrets = {
-      tailscaleAuthKey = {
-        file = ../../secrets/tailscale-authkey.age;
-        owner = config.home.username;
-      };
 
-      syncthing-cert = {
-        file = ${inputs.secrets}/syncthing-cert-ubuntu.age;
-        path = "${config.home.homeDirectory}/.config/syncthing/cert.pem";
-      };
+    syncthing-cert = {
+      file = "${inputs.secrets}/syncthing-cert-ubuntu.age";
+      path = "${config.home.homeDirectory}/.config/syncthing/cert.pem";
+    };
 
-      syncthing-key = {
-        file = ${inputs.secrets}/syncthing-key-ubuntu.age;
-        path = "${config.home.homeDirectory}/.config/syncthing/key.pem";
-      };
+    syncthing-key = {
+      file = "${inputs.secrets}/syncthing-key-ubuntu.age";
+      path = "${config.home.homeDirectory}/.config/syncthing/key.pem";
+    };
   };
 
   home.file.".config/syncthing/.keep".text = "";
