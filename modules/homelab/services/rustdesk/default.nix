4b9dd1e2d6ad65228c0c1db00bdfcaf6f2740984
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-08-30 15:12:55 -0400

    updated monitoredServices for rustdesk

--- modules/homelab/services/rustdesk/default.nix
+++ modules/homelab/services/rustdesk/default.nix
@@ -14,6 +14,14 @@ in
       default = "rustdesk.${homelab.baseDomain}";
       description = "Hostname RustDesk clients use to reach the self-hosted signal/relay server";
     };
+    monitoredServices = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "rustdesk-relay"
+        "rustdesk-signal"
+      ];
+      description = "Actual systemd unit names created by services.rustdesk-server - the motd service table checks these instead of the nonexistent 'rustdesk' unit";
+    };
   };
   config = lib.mkIf cfg.enable {
     services.rustdesk-server = {
