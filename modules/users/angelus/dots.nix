f0992e563e2ecf48ab0bc32a8950a7f043139cb9
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-09-03 08:58:50 -0400

    disabled Tailnet Tray

--- modules/users/angelus/dots.nix
+++ modules/users/angelus/dots.nix
@@ -1,4 +1,4 @@
-{  config, lib, ... }:
+{ config, lib, ... }:
 let
   home = {
     username = "angelus";
@@ -12,57 +12,57 @@ in
 
   imports = [
     ./gitconfig.nix
-      ../../dots/cosmic/default.nix
-      ../../dots/neofetch/default.nix
-      ../../dots/ssh/default.nix
-      ../../dots/zsh/default.nix
-      ../../dots/bitwarden/default.nix
-      ../../dots/firefox/default.nix
-      ../../dots/ghostty/default.nix
-      #../../dots/librewolf/default.nix
-      ../../dots/tssystray/default.nix
-      #../../dots/variety/default.nix
-      ../../dots/vscodium/default.nix
-      ../../dots/wallpaper/default.nix
-      #g../../dots/wpaperd/default.nix
-      ../../dots/zed/default.nix
+    ../../dots/cosmic/default.nix
+    ../../dots/neofetch/default.nix
+    ../../dots/ssh/default.nix
+    ../../dots/zsh/default.nix
+    ../../dots/bitwarden/default.nix
+    ../../dots/firefox/default.nix
+    ../../dots/ghostty/default.nix
+    #../../dots/librewolf/default.nix
+    #../../dots/tssystray/default.nix
+    #../../dots/variety/default.nix
+    ../../dots/vscodium/default.nix
+    ../../dots/wallpaper/default.nix
+    #g../../dots/wpaperd/default.nix
+    ../../dots/zed/default.nix
   ];
 
-    options.myHomeDots = {
-        enableGui = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Enable GUI-specific dotfiles like desktop apps and window utilities.";
-        };
-        enableCore = lib.mkOption {
-          type = lib.types.bool;
-          default = true;
-          description = "Enable core/headless terminal dotfiles needed everywhere.";
-        };
-      };
-
-  config = {
-  #  nixpkgs = {
-  #    overlays = [ ];
-  #    config = {
-  #      allowUnfree = true;
-  #      allowUnfreePredicate = (_: true);
-  #      permittedInsecurePackages = [ ##COMEBACKTOTHIS##
-  #              "electron-39.8.10"
-  #      ];
-        
-  #  };
-  #};
-
-  home = home;
-
-  programs.nix-index = {
-    enable = true;
-    enableZshIntegration = true;
+  options.myHomeDots = {
+    enableGui = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable GUI-specific dotfiles like desktop apps and window utilities.";
+    };
+    enableCore = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable core/headless terminal dotfiles needed everywhere.";
+    };
   };
 
-  programs.home-manager.enable = true;
+  config = {
+    #  nixpkgs = {
+    #    overlays = [ ];
+    #    config = {
+    #      allowUnfree = true;
+    #      allowUnfreePredicate = (_: true);
+    #      permittedInsecurePackages = [ ##COMEBACKTOTHIS##
+    #              "electron-39.8.10"
+    #      ];
 
-  systemd.user.startServices = "sd-switch";
+    #  };
+    #};
+
+    home = home;
+
+    programs.nix-index = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    programs.home-manager.enable = true;
+
+    systemd.user.startServices = "sd-switch";
   };
 }
