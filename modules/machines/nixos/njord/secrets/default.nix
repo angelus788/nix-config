8418f1354595a4fb699cd3cc6c1585844a4782e3
Emilio S <3382264+angelus788@users.noreply.github.com>
2026-09-07 16:57:42 -0400

    added syncthing secrets to njord

--- modules/machines/nixos/njord/secrets/default.nix
+++ modules/machines/nixos/njord/secrets/default.nix
@@ -7,8 +7,8 @@
     #duckDNSToken.file = "${inputs.secrets}/duckDNSToken.age";
     #hashedPasswordFile.file = "${inputs.secrets}/hashedUserPassword.age";
     #initialHashedPassword.file = "${inputs.secrets}/initialHashedPassword.age";
-    syncthing-cert.file = "${inputs.secrets}/syncthing-cert-mayra.age";
-    syncthing-key.file = "${inputs.secrets}/syncthing-key-mayra.age";
+    syncthing-cert.file = "${inputs.secrets}/syncthing-cert-njord.age";
+    syncthing-key.file = "${inputs.secrets}/syncthing-key-njord.age";
     netbirdSetupKey.file = "${inputs.secrets}/netbirdSetupKey.age";
   };
 }
